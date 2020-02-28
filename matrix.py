ef32f22a3bc9d2cfa9cc2c6a7c740c60ce07f7f2
udayakumaran <43293496+udayakumaran@users.noreply.github.com>
2020-02-28 17:00:58 +0600

    Add files via upload

--- matrix.py
+++ matrix.py
@@ -1,19 +1,36 @@
 r = int(input())
-c=int(input())
+c = int(input())
+r1 = int(input())
+c1 = int(input())
 mat = []
-mat1 =[]
+mat1 = []
+u=0
 for i in range(r):
-    a=[]
+    a = []
     for j in range(c):
         a.append(int(input()))
     mat.append(a)
-for i in range(r):
-    b=[]
-    for j in range(c):
+for i in range(r1):
+    b = []
+    for j in range(c1):
         b.append(int(input()))
     mat1.append(b)
 
 for i in range(r):
-    for j in range(c):
-        print(mat[i][j] + mat1[i][j],end=" ")
+    c2 = []
+    for j in range(c1):
+        c2.append(0)
+    r.append(c2)
+
+for i in range(len(mat)):
+    for j in range(len(mat1[0])):
+        for k in range(len(mat1)):
+             r[i][j] += mat[i][k] * mat1[k][j]
     print()
+for h in r:
+   print(h)
+
+
+
+
+
